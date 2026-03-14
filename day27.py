9ad3a5655aad286d4f4e2f0346c20f407d1185ad
alifarhan39r <alifarhan39r@gmail.com>
2026-03-14 14:48:12 +0500

    Inital commit

---
+++ day27.py
@@ -0,0 +1,9 @@
+letter="hey my name is {} and i am from {}"
+name="Farhan"
+country="Pakistan"
+# print(letter.format(name,country))
+# print(f"hey my name is {{name}} and i am from {{country}}")
+# price=49.8796878
+# txt=f"for only {price:.2f} in Pakistan"
+# print(txt)
+print(f"{2*30}")
