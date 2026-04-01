b0d7770b6e43b28532790a35b04803913cc7c4dc
alifarhan39r <alifarhan39r@gmail.com>
2026-04-01 12:25:15 +0500

    initial commit

---
+++ day35.py
@@ -0,0 +1,15 @@
+# for i in range(5):
+# for i in []:
+#     print(i)
+# else:
+#     print("sorry no i")
+ 
+i=0
+while(i<7):
+    print(i)
+    i=i+1
+    i==4
+    print('found')
+    break
+else:
+    print('not found')
