20cbac448873c21ba18f6b3eda364d81401cd92b
alifarhan39r <alifarhan39r@gmail.com>
2026-04-14 11:17:14 +0500

    initial commit

---
+++ day36.py
@@ -0,0 +1,9 @@
+a=input("enter the number:=")
+print(f"Multiplication of {a} is:")
+try:
+  for i in range(1,11):
+    print(f"{int(a)}X{i}={int(a)*i}")
+except:
+  print("sorry this is error")
+
+print("s")
